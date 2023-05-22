1bea8d2ff3ecde7620c65167768b2beeab04749b
Dorabang <psa1195@naver.com>
2023-05-22 16:03:19 +0900

    feat: metadata 수정 및 footer 작성

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import '@/app/globals.css';
 import { Noto_Sans_KR } from 'next/font/google';
 import Link from 'next/link';
+import { AiOutlineSetting } from 'react-icons/ai';
 
 const notoSansKR = Noto_Sans_KR({
   weight: ['400', '700', '900'],
@@ -8,8 +9,8 @@ const notoSansKR = Noto_Sans_KR({
 });
 
 export const metadata = {
-  title: 'webSite',
-  description: 'codeit Next.js webSite clone',
+  title: 'Undefined',
+  description: 'codeit Next.js webSite clone, Next.js | typeScript | Tailwind',
 };
 
 export default function RootLayout({
@@ -25,10 +26,20 @@ export default function RootLayout({
             <h1 className='text-3xl font-extrabold tracking-tight text-neutral-900'>
               <Link href='/'>Undefined</Link>
             </h1>
-            <div></div>
+            <div>
+              <AiOutlineSetting size={24} />
+            </div>
           </div>
         </header>
         {children}
+        <footer className='bg-neutral-900 py-10 mt-20'>
+          <div className='container mx-auto'>
+            <address className='not-italic text-neutral-400'>
+              (FrontEnd Developer) Park Su-a <br />
+              Next.js | TypeScript | TailwindCSS
+            </address>
+          </div>
+        </footer>
       </body>
     </html>
   );
