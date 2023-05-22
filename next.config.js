5388cdf56874e68a84f0838cb97fc061b4833e62
Dorabang <psa1195@naver.com>
2023-05-22 21:58:48 +0900

    feat: redirects 설정

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/items/:id',
+        destination: '/products/:id',
+        permanent: true, // response status code, 웹 브라우저에 주소가 바뀌었다는 사실 저장 여부
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
