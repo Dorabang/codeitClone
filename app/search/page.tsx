158e839854c6cfb202ac2ad4f7e4bd9ad98f900b
Dorabang <psa1195@naver.com>
2023-05-22 16:08:07 +0900

    feat: 검색 정보에 따른 검색 결과 리스트 추가

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,16 +1,32 @@
 'use client';
 import { useSearchParams } from 'next/navigation';
 import SearchForm from '../components/SearchForm';
+import ProductList from '../components/ProductList';
+import { useEffect, useState } from 'react';
+import axios from '../libs/axios';
 
 const SearchPage = () => {
   const searchParams = useSearchParams();
   const q = searchParams.get('q');
+
+  const [products, setProducts] = useState<any[]>([]);
+
+  const getProducts = async (query: string | null) => {
+    const res = await axios.get(`/products/?q=${query}`);
+    const nextProducts = res.data.results;
+    setProducts(nextProducts);
+  };
+
+  useEffect(() => {
+    getProducts(q);
+  }, [q]);
 
   return (
     <div>
       <p>search</p>
       <SearchForm initialValue={`${q}`} />
       {q && <p>{q} 검색 결과</p>}
+      <ProductList products={products} />
     </div>
   );
 };
