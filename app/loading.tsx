323d5b1d3a5ca5cb4312ac29863adb182825cf2b
Dorabang <psa1195@naver.com>
2023-05-22 22:05:48 +0900

    design: 로딩 페이지 css 설정

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,7 +1,9 @@
 const LoadingPage = () => {
   return (
     <>
-      <p>Loading...</p>
+      <div className='text-center flex justify-center items-center h-screen'>
+        <div className='animate-spin rounded-full w-10 h-10 border-4 border-solid border-neutral-900/10 border-l-neutral-900 dark:border-white/10 dark:border-l-neutral-50'></div>
+      </div>
     </>
   );
 };
